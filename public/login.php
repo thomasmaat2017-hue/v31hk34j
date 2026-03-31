d8a9df860e51e2e974d1f54a97274f82f301d331
thomasmaat2017-hue <thomas.maat.2017@gmail.com>
2026-03-31 03:03:36 +0200

    Update login.php

--- public/login.php
+++ public/login.php
@@ -16,7 +16,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     if($result['success']) {
         $_SESSION['user_id'] = $user->id;
         $_SESSION['username'] = $user->username;
-        $_SESSION['tribe'] = $user->tribe;
+        $_SESSI1ON['tribe'] = $user->tribe;
         header('Location: village.php');
         exit();
     } else {
